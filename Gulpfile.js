bdc85724ea9a859060a22104d16c5621b88c591b
Ed Lomonaco <maggot05@gmail.com>
2017-01-04 22:57:35 -0500

    WIP Theme setup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
+var cleanCss = require('gulp-clean-css');
 var gulpConcat = require('gulp-concat');
 var gulpUglify = require('gulp-uglify');
 var pump = require('pump');
 
-gulp.task('build-dist', function (cb) {
+gulp.task('build-dist', function () {
+    pump([
+        gulp.src('jquery.btnswitch.css'),
+        cleanCss(),
+        gulpConcat('jquery.btnswitch.min.css'),
+        gulp.dest('dist')
+    ]);
+
     pump([
         gulp.src('jquery.btnswitch.js'),
             gulpConcat('jquery.btnswitch.min.js'),
             gulpUglify(),
             gulp.dest('dist')
-    ], cb);
+    ]);
 });
 
 gulp.task('default', ['build-dist']);
